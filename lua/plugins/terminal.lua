6af43e0d40e4dc42b9989289e25a3881b35a2cb7
eddysanoli <eddysanoli@gmail.com>
2024-06-08 21:43:53 -0600

    Description Fixes and NVIM Lightbulb
    Changes:
    - Installed nvim-lightbulb to avoid having to check in all lines if code
    actions are available. Now a little lightbulb will actually show when
    they are available
    - Removed descriptions from terminal keymaps. They are not needed
    - Formatted the LSP config
    - Renamed "init.lua" to other.lua, since those are other packages that
    can be grouped together. I might split them later cause I dont really
    like the "other" approach.

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -53,13 +53,13 @@ return {
 
 			function _G.set_terminal_keymaps()
 				local opts = { buffer = 0 }
-				vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], opts, { desc = "Exit terminal mode" })
-				vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts, { desc = "Exit terminal mode" })
-				vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts, { desc = "Move to the left window" })
-				vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts, { desc = "Move to the bottom window" })
-				vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts, { desc = "Move to the top window" })
-				vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts, { desc = "Move to the right window" })
-				vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts, { desc = "Exit terminal mode and move to the next window" })
+				vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], opts)
+				vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+				vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+				vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+				vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+				vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+				vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
 			end
 
 			vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
