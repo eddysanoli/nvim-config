6722eb5510694fa2eec7aab52a34f9e3fc1af568
eddysanoli <eddysanoli@gmail.com>
2024-06-09 13:00:21 -0600

    Separating Other.lua
    Took out "lexima" and "pyenv-pyright" from other.lua into their own
    files. Since the plugins that remained were basically all for motions, I
    renamed the file motions.lua. Also installed a new motion called
    vim-sneak

---
+++ lua/plugins/motions.lua
@@ -0,0 +1,6 @@
+return {
+	"chaoren/vim-wordmotion", -- When using word motions, camel case and underscored words are treated accordingly
+	"tpope/vim-surround", -- Surround text with brackets, quotes, etc.
+    "justinmk/vim-sneak", -- Jump to a new location using two characters. Analog to the "f" motion
+	"easymotion/vim-easymotion", -- Show visible landmarks for easier navigation
+}
---
+++ lua/plugins/pyenv-pyright.lua
@@ -0,0 +1,3 @@
+return {
+	"alefpereira/pyenv-pyright",
+}
