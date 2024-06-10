040c8ba08e346020a5066f73cca74f17e8de603c
eddysanoli <eddysanoli@gmail.com>
2024-06-10 00:35:36 -0600

    Better Python Integration
    Fought quite a lot to get python properly working in neovim, and after a while its almost perfect. Ruff is now properly working, just had to activate a virtual environment first before installing things. If I install it in one environment, if any other has the necessary dependencies, it will work. Thats how I made 3.8.10 work properly after I installed everything on 3.10. Also added more formatting and diagnostic tools to none ls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,64 +1,95 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup({
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup({
                 ui = {
                     border = "rounded",
-                }
+                },
             })
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup()
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-	},
-	{
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v3.x",
-		config = function()
-			local lsp_zero = require("lsp-zero")
-			lsp_zero.extend_lspconfig()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup()
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+    },
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v3.x",
+        config = function()
+            local lsp_zero = require("lsp-zero")
+            lsp_zero.extend_lspconfig()
+            lsp_zero.on_attach(function(_, bufnr)
+                lsp_zero.default_keymaps({ buffer = bufnr })
+            end)
 
-			lsp_zero.on_attach(function(_, bufnr)
-				lsp_zero.default_keymaps({ buffer = bufnr })
-			end)
+            require("mason").setup({})
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "bashls",
+                    "cssls",
+                    "dotls",
+                    "intelephense",
+                    "html",
+                    "tsserver",
+                    "vimls",
 
-			require("mason").setup({})
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"bashls",
-					"cssls",
-					"dockerls",
-					"docker_compose_language_service",
-					"dotls",
-					"intelephense",
-					"html",
-					"jsonls",
-					"tsserver",
-					"vimls",
-					"yamlls",
-				},
-				handlers = {
-					function(server_name)
-						require("lspconfig")[server_name].setup({})
-					end,
-				},
-			})
+                    -- Docker
+                    "dockerls",
+                    "docker_compose_language_service",
 
-			-- Add mapping for code actions
-			-- For more mappings see this: https://lsp-zero.netlify.app/v3.x/reference/lua-api.html#lsp-actions
-			vim.keymap.set("n", "<leader>ca", function()
-				vim.lsp.buf.code_action()
-			end, { desc = "Show code actions" })
+                    -- Markup Languages
+                    "jsonls",
+                    "yamlls",
+                    "taplo",
 
-			local bufnr = vim.fn.bufnr("%")
-			vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<cr>", { buffer = bufnr, desc = "Show references" })
-		end,
-	},
+                    -- Python
+                    "ruff",
+                    "pylsp",
+                },
+                handlers = {
+                    function(server_name)
+                        local server = require("lspconfig")[server_name]
+                        if server_name == "pylsp" then
+                            server.setup({
+                                settings = {
+                                    pylsp = {
+                                        plugins = {
+                                            ruff = {
+                                                enabled = true,
+                                                formatEnabled = true,
+                                                lineLength = 91,
+                                            },
+                                            pycodestyle = { enabled = false },
+                                            pyflakes = { enabled = false },
+                                            mccabe = { enabled = false },
+                                            autopep8 = { enabled = false },
+                                            yapf = { enabled = false },
+                                        },
+                                    },
+                                },
+                            })
+                            return
+                        else
+                            server.setup({})
+                        end
+                    end,
+                },
+            })
+
+            -- Add mapping for code actions
+            -- For more mappings see this: https://lsp-zero.netlify.app/v3.x/reference/lua-api.html#lsp-actions
+            vim.keymap.set("n", "<leader>ca", function()
+                vim.lsp.buf.code_action()
+            end, { desc = "Show code actions" })
+
+            local bufnr = vim.fn.bufnr("%")
+            vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<cr>", { buffer = bufnr, desc = "Show references" })
+        end,
+    },
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,20 +1,24 @@
 return {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-        "nvimtools/none-ls-extras.nvim",
-    },
-    config = function()
-        local null_ls = require("null-ls")
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.prettier,
-                null_ls.builtins.completion.spell,
-                require("none-ls.diagnostics.eslint_d"),
-                -- Don't re-add it codespell here. It fails. Please
-            },
-        })
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.completion.spell,
+				null_ls.builtins.code_actions.refactoring,
+				null_ls.builtins.diagnostics.mypy,
+				null_ls.builtins.formatting.djhtml,
+				null_ls.builtins.formatting.isort,
+				require("none-ls.diagnostics.eslint_d"),
+				-- Don't re-add it codespell here. It fails. Please
+			},
+		})
 
-        vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, { desc = "Format the current file" })
-    end,
+		vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, { desc = "Format the current file" })
+	end,
 }
