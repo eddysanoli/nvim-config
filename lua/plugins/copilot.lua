27aa5a37ef6c837b24ae0d2232b2f361cab16b05
eddysanoli <eddysanoli@gmail.com>
2024-06-09 18:38:02 -0600

    Installing Copilot Chat
    I chat with github copilot way too much. Installed it so that I can ask it a couple of things. Included its keymaps in the cheatsheet in the previous commit

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,20 @@
 return {
-    "github/copilot.vim",
+	{
+		"github/copilot.vim",
+	},
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "canary",
+		dependencies = {
+			{ "github/copilot.lua" },
+			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+		},
+		config = function()
+			require("CopilotChat").setup({
+				debug = true, -- Enable debugging
+			})
+			vim.keymap.set("n", "<leader>cc", ":CopilotChatOpen<CR>", { silent = true, desc = "Open Copilot Chat" })
+			vim.keymap.set("n", "<leader>cx", ":CopilotChatClose<CR>", { silent = true, desc = "Close Copilot Chat" })
+		end,
+	},
 }
