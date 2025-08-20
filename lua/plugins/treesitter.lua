67866f91792c29da0923aee6a9803f194e36f9ed
eddysanoli <eddysanoli@gmail.com>
2025-08-20 12:19:38 -0600

    Improved Treesitter Config
    Just discovered that you can do fancy stuff with treesitter if you add some modules, so I added incremental selection and textobjects. Dont know if Ill ever use incremental selection, but text objects look so cool. You can use them to select functions, and other things, just like you would something like select stuff within some parenthesis for example. I like it. Also added the treesitter context plugin to always show what the code refers to when you are nested within a scope.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,51 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local config = require("nvim-treesitter.configs")
-        config.setup({
-            auto_install = true, -- Automatically install parsers for languages treesitter doesn't know
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end,
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		dependencies = {
+			'nvim-treesitter/nvim-treesitter-textobjects',
+            'nvim-treesitter/nvim-treesitter-context',
+		},
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				auto_install = true, -- Automatically install parsers for languages treesitter doesn't know
+				highlight = { enable = true },
+				indent = { enable = true },
+				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<leader>ss",
+						node_incremental = "<leader>si",
+						scope_incremental = "<leader>sc",
+						node_decremental = "<leader>sd",
+					},
+				},
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+							["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+						},
+						selection_modes = {
+							["@parameter.outer"] = "v", -- charwise
+							["@function.outer"] = "V", -- linewise
+							["@class.outer"] = "<c-v>", -- blockwise
+						},
+						include_surrounding_whitespace = true,
+					},
+				},
+                context = {
+                    enable = true,
+                    max_lines = 1,
+                },
+			})
+		end,
+	},
 }
