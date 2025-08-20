d33db1d765af97b9e21558c31bc912f7b678cc5f
eddysanoli <eddysanoli@gmail.com>
2025-08-20 12:17:41 -0600

    Customizing Breadcrumbs
    Breadcrumbs were a bit too wordy for my liking, so I finally figured out how to reduce the amount of symbols used. Also removed breadcrumbs from terminals, although the empty space for the winbar is still left at the top even though I would like to remove it

--- lua/plugins/breadcrumbs.lua
+++ lua/plugins/breadcrumbs.lua
@@ -1,27 +1,45 @@
 return {
-	"Bekaboo/dropbar.nvim",
-	-- optional, but required for fuzzy finder support
-	dependencies = {
-		"nvim-telescope/telescope-fzf-native.nvim",
-		build = "make",
-	},
-    opts = {
-        sources = {
-            path = {
-                max_depth = 4,
-            },
-            lsp = {
-                max_depth = 2,
-            },
-            treesitter = {
-                max_depth = 2,
-            },
-        }
+    "Bekaboo/dropbar.nvim",
+    -- optional, but required for fuzzy finder support
+    dependencies = {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
     },
-	config = function()
-		local dropbar_api = require("dropbar.api")
-		vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
-		vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
-		vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
-	end,
+    config = function()
+        local dropbar_api = require("dropbar.api")
+        require("dropbar").setup({
+            sources = {
+                lsp = {
+                    max_depth = 2,
+                },
+                treesitter = {
+                    max_depth = 2,
+                },
+                path = {
+                    max_depth = 3,
+                },
+                markdown = {
+                    max_depth = 3,
+                },
+                terminal = {
+                    show_current = false,
+                    icon = "",
+                    name = function()
+                        return nil
+                    end
+                },
+            },
+            bar = {
+                hover = true,
+                padding = {
+                    left = 2,
+                    right = 2,
+                }
+            }
+        })
+
+        vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
+        vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
+        vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
+    end,
 }
